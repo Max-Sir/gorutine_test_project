bed2cba7183f1e283fac0d0cfdf9eb2ab0a4ef63
Maxim <syromolotov.maks@mail.ru>
2021-05-24 22:52:27 +0300

    start of writing part Sum using gorutines

--- main/main.go
+++ main/main.go
@@ -2,19 +2,39 @@ package main
 
 import "fmt"
 
-func rm(a int,ch chan int){
-	total:=0
+func partSum(a int, sum chan int, cur chan int, prev chan int) {
+	cur <- a
+	for i := <-prev; i < <-cur; i++ {
+		sum <- <-sum + i
+	}
+	prev <- a
+}
+
+func sumFromRange(a int, b int) int {
+	cur := make(chan int)
+	sum := make(chan int)
+	prev := make(chan int)
+	prev <- a
+	for i := a; i < b; i += 1 {
+		go partSum(i, sum, cur, prev)
+	}
+	return <-sum
+}
+
+func rm(a int, ch chan int) {
+	total := 0
 	for i := 0; i < a; i++ {
-		total+=i
+		total += i
 		fmt.Println(i)
 	}
-	ch<-total
+	ch <- total
 }
 
 func main() {
-	ch1:=make(chan int)
-	ch2:=make(chan int)
-	go rm(9,ch1)
-	go rm(123,ch2)
+	fmt.Println(sumFromRange(0, 10))
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go rm(9, ch1)
+	go rm(123, ch2)
 	fmt.Println(<-ch1)
 }
